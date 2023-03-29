1b35d0eaf8c3b72eb23955406a7da62286af27b5
Safi <safi.sharieff@gmail.com>
2023-03-29 23:27:42 +0400

    minor changes

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ import config
 
 reddit = praw.Reddit(client_id=config.client_id, client_secret=config.client_secret, user_agent=config.user_agent)
 
-subreddit = reddit.subreddit('pics')
+subreddit = reddit.subreddit('nosleep')
 
+for submission in subreddit.top(limit=1):
+    print(submission.selftext)
 
-
-# print(subreddit.display_name)
-# print(subreddit.title)
-# print(subreddit.description)
-
-# print(reddit.read_only)
