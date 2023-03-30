97a6ec54f4dd606b690a1ebee8cc7ab8066719ae
Safi <safi.sharieff@gmail.com>
2023-03-30 23:22:00 +0400

    Adding a flask app to create a web interface for the project

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import praw
 import config
+from nrclex import NRCLex
 
 reddit = praw.Reddit(client_id=config.client_id, client_secret=config.client_secret, user_agent=config.user_agent)
 
-subreddit = reddit.subreddit('nosleep')
+bigString = ''
+subreddit = reddit.subreddit('allthingsgucci')
 
-for submission in subreddit.top(limit=1):
-    print(submission.selftext)
+for submission in subreddit.top(limit=100):
+    bigString += submission.selftext
+
+text_stuff = NRCLex(bigString)
+results = text_stuff.raw_emotion_scores
+
+print(results)
 
