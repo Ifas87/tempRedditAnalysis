7bcb4df8fd54193d4993ed002a90a15ff9cb618c
Safi <safi.sharieff@gmail.com>
2023-04-02 17:18:03 +0400

    Added new URL routes and repackaged the NLTK code

--- index.py
+++ index.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import render_template
+from main import reddit, data_loading, all_analysis
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '845ea0b38cc7bd5b7520d5bd54bb2e610008319c51b2ec15'
+
 
 @app.route('/')
 def index():
-    return("Hello World")
+    return render_template("index.html")
+
+
+@app.route('/request')
+def handle_request():
+    pass
+
+
+@app.route('/results')
+def report():
+    pass
 
 
 if (__name__ == '__main__'):
--- main.py
+++ main.py
@@ -5,13 +5,34 @@ from nrclex import NRCLex
 reddit = praw.Reddit(client_id=config.client_id, client_secret=config.client_secret, user_agent=config.user_agent)
 
 bigString = ''
-subreddit = reddit.subreddit('allthingsgucci')
+subreddit = reddit.subreddit('LeopardsAteMyFace')
 
 for submission in subreddit.top(limit=100):
-    bigString += submission.selftext
+    bigString += submission.title
 
 text_stuff = NRCLex(bigString)
 results = text_stuff.raw_emotion_scores
 
 print(results)
+
+
+def data_loading(subname, imageSubs=False):
+    bigString = ''
+    subreddit = reddit.subreddit(subname)
+
+    if imageSubs:
+        for submission in subreddit.top(limit=100):
+            bigString += submission.title
+    else:
+        for submission in subreddit.top(limit=100):
+            bigString += submission.selftext
+    
+    return bigString
+
+
+def all_analysis(sampleString):
+    text_stuff = NRCLex(bigString)
+    results = text_stuff.raw_emotion_scores
+
+    return results
 
