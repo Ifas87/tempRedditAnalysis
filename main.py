769abf84a19f1c6f7e4d9b66a814bdfde4adbbad
Safi <safi.sharieff@gmail.com>
2023-04-04 21:48:42 +0400

    Minor changes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import praw
 import config
-from nrclex import NRCLex 
+from nrclex import NRCLex
+import sumy.summarizers.lex_rank as su
+import sumy.parsers.plaintext as pt
+
 
 reddit = praw.Reddit(client_id=config.client_id, client_secret=config.client_secret, user_agent=config.user_agent)
 
@@ -24,3 +27,9 @@ def all_analysis(sampleString):
 
     return results
 
+if __name__ == "__main__":
+    strs = data_loading(subname='pics', imageSubs=True)
+    summariser = su.LexRankSummarizer(strs, 10)
+
+    for sentence in summariser:
+        print(sentence)
