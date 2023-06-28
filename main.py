fbcd1e03912f842cb2f6f1d7b38bb27629835232
Safi <safi.sharieff@gmail.com>
2023-06-28 11:59:14 +0400

    Added the word cloud and emotioal analysis graphs

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import config
 from nrclex import NRCLex
 import sumy.summarizers.lex_rank as su
 import sumy.parsers.plaintext as pt
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 
 reddit = praw.Reddit(client_id=config.client_id, client_secret=config.client_secret, user_agent=config.user_agent)
@@ -17,7 +19,6 @@ def data_loading(subname, imageSubs=False):
     else:
         for submission in subreddit.top(limit=100):
             bigString += submission.selftext
-    
     return bigString
 
 
@@ -25,13 +26,25 @@ def all_analysis(sampleString):
     text_stuff = NRCLex(sampleString)
     results = text_stuff.raw_emotion_scores
 
+    labels = list(results.keys())
+    data = list(results.values())
+
+    plt.figure(figsize=(10, 3))
+    plt.bar(range(len(results)), data, tick_label=labels)
+    plt.show()
+
+    wordcloud = WordCloud().generate(sampleString)
+
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
     return results
 
 
 if __name__ == "__main__":
-    strs = data_loading(subname='pics', imageSubs=True)
-    summariser = su.LexRankSummarizer(strs, 10)
+    strs = all_analysis(data_loading(subname='pics', imageSubs=True))
+    # summariser = su.LexRankSummarizer(strs, 10)
 
-    for sentence in summariser:
-        print(sentence)
+    # for sentence in summariser:
+    #     print(sentence)
  
